2d972ea053c50d40f68dacaad4834fc3f1382e5c
Dom <dom@itsallbroken.com>
2021-02-08 16:54:09 +0000

    WIP: feat: FreeBSD 12 PMC support
    Works towards having pmc-sys work on FreeBSD 12, which appears to be
    using C++ somewhere in the PMC stack.
    
    Links against libc++, but fails at runtime with:
    
    		Running target/debug/deps/pmc-a0be449f3957c7e2
    	ld-elf.so.1: /usr/lib/libpmc.so.5: Undefined symbol "__gxx_personality_v0"
    	error: test failed, to rerun pass '--test pmc'

--- build.rs
+++ build.rs
@@ -4,17 +4,22 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-lib=pmc");
+    println!("cargo:rustc-link-lib=dylib=pmc");
+    println!("cargo:rustc-link-lib=dylib=c++");
+    println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        // .clang_arg("-x")
+        // .clang_arg("c++")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .blacklist_type("^__max_align_t$")
         .generate()
-        .expect("Unable to generate bindings");
+        .expect("unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("failed to write generated bindings");
 }
